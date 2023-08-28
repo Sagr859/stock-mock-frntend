8a4a3326244ca9f64069c12b967949c77e711888
Sagar Sajeev <sagaramss@gmail.com>
2023-08-28 13:01:40 +0530

    newer updates for deploy

--- src/endpoint.js
+++ src/endpoint.js
@@ -1,4 +1,5 @@
-export const BASE_URL = process.env.REACT_APP_API_URL
+export const BASE_URL = "https://sagarstockmockbknd.vercel.app/"
+//process.env.REACT_APP_API_URL
 //`http://localhost:8000/`
 
 export const LIST_STOCKS = BASE_URL + 'api/stocks'
